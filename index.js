c8562aa643997a88833a0bf99231cf558c721b8d
ivantodorovich <ivan.todorovich@gmail.com>
2014-05-19 11:55:22 -0300

    Recognize LogType
    01: Fingerprint 1
    02: Fingerprint 2
    04: Password log

--- index.js
+++ index.js
@@ -10,12 +10,12 @@ module.exports.readFileSync = function (filename) {
 	for (i=0;i<n;i++) {
 		var OFFSET = 3+i*14
 		result.push({
-			_unk1_: [ buffer[OFFSET], buffer[OFFSET+1] ],
-			userId: int24.readUInt24BE(buffer, OFFSET+2),
-			time:   new Date(BASEDATE.getTime()+buffer.readUInt32BE(OFFSET+5)*1000),
-			_unk2_: [ buffer[OFFSET+9] ],
-			code:   buffer.readUInt8(OFFSET+10),
-			jobId:  int24.readUInt24BE(buffer, OFFSET+11)
+			unknown: [ buffer[OFFSET], buffer[OFFSET+1] ],
+			userId:  int24.readUInt24BE(buffer, OFFSET+2),
+			time:    new Date(BASEDATE.getTime()+buffer.readUInt32BE(OFFSET+5)*1000),
+			type:    buffer.readUInt8(OFFSET+9),
+			code:    buffer.readUInt8(OFFSET+10),
+			jobId:   int24.readUInt24BE(buffer, OFFSET+11)
 		})
 	}
 	return result
