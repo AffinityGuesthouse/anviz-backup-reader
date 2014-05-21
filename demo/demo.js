f52d6766718df64832982e8a3238cb07409955c6
ivantodorovich <ivan.todorovich@gmail.com>
2014-05-21 15:22:18 -0300

    Stream Support and Async methods

--- demo/demo.js
+++ demo/demo.js
@@ -1,3 +1,20 @@
+var fs     = require('fs')
 var reader = require('../index.js')
-var records = reader.readFileSync('./BAK.KQ')
-console.log(records)
+var JSONStream = require('JSONStream') // WARN: Must be installed separately, it's not a dependency.
+
+var filename = './BAK.KQ'
+
+// Sync
+var records = reader.readFileSync(filename)
+console.log('Sync: ', records)
+
+// Async
+reader.readFile(filename, function(records) {
+    console.log('Async: ', records)
+})
+
+// Stream
+fs.createReadStream(filename)
+.pipe(reader.Stream())
+.pipe(JSONStream.stringify())
+.pipe(process.stdout)
--- index.js
+++ index.js
@@ -1,8 +1,16 @@
-var fs    = require('fs')
-var int24 = require('int24')
+var fs        = require('fs')
+var int24     = require('int24')
+var Transform = require('stream').Transform
+var util      = require('util')
 
 var BASEDATE = new Date(2000,0,2)
 
+/**
+ * Reads a BAK.KQ file and returns an array of records.
+ * Works for small files. It will block and use a lot of memory for big files.
+ * @param  {String}   filename
+ * @return {Array}    records
+ */
 module.exports.readFileSync = function (filename) {
 	var buffer = fs.readFileSync(filename)
 	var n = int24.readUInt24BE(buffer,0)
@@ -19,4 +27,77 @@ module.exports.readFileSync = function (filename) {
 		})
 	}
 	return result
+}
+
+/**
+ * Reads a BAK.KQ file and parses its contents.
+ * If rowCallback is supplied, each row will sent to that callback.
+ * If not supplied, it will send an Array to callback, but it is more expensive.
+ * @param  {String}   filename
+ * @param  {Function} rowCallback (Optional)
+ * @param  {Function} callback
+ */
+module.exports.readFile = function (filename, rowCallback, endCallback) {
+    if (typeof rowCallback !== 'function') throw new TypeError('Callback is required!')
+    if (typeof endCallback !== 'function') {
+        endCallback = rowCallback
+        rowCallback = null
+        var _data   = []
+    }
+
+    fs.createReadStream(filename)
+    .pipe(Stream())
+    .on('data', function(data) {
+        if (!!rowCallback) rowCallback(data)
+        else _data.push(data)
+    })
+    .on('end', function() {
+        if (!!rowCallback) endCallback()
+        else endCallback(_data)
+    })
+}
+
+/* Stream 
+ * ======
+ */
+
+module.exports.Stream = Stream
+util.inherits(Stream, Transform)
+
+function Stream (options) {
+    if (this instanceof Stream === false) return new Stream(options)
+    Transform.call(this)
+    this._readableState.objectMode = true
+    this._length = null
+    this._buffer = Buffer(0)
+}
+
+Stream.prototype._transform = function (chunk, encoding, done) {
+
+    this._buffer = Buffer.concat([ this._buffer, chunk ], this._buffer.length + chunk.length)
+    
+    // Read Header
+    if (this._length === null) {
+        if (this._buffer.length < 3) return done()
+        this._length = int24.readUInt24BE(this._buffer, 0)
+        this._buffer = this._buffer.slice(3)
+        this.emit('header', this._length)
+    }
+
+    // Records
+    var n = parseInt(this._buffer.length/14)
+    for (i=0;i<n;i++) {
+        var OFFSET = i*14
+        var record = {
+            unknown: [ this._buffer[OFFSET], this._buffer[OFFSET+1] ],
+            userId:  int24.readUInt24BE(this._buffer, OFFSET+2),
+            time:    new Date(BASEDATE.getTime()+this._buffer.readUInt32BE(OFFSET+5)*1000),
+            type:    this._buffer.readUInt8(OFFSET+9),
+            code:    this._buffer.readUInt8(OFFSET+10),
+            jobId:   int24.readUInt24BE(this._buffer, OFFSET+11)
+        }
+        this.push(record)
+    }
+
+    done()
 }
